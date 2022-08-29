6ddc3f51fa256929142c5479dd2f2d608afba48b
IjazkhanAfridi <iafridi640@gmail.com>
2022-08-29 17:31:34 +0500

    first commit

---
+++ js/color.js
@@ -0,0 +1,19 @@
+// const dark=document.querySelectorAll(".dark"),
+//     totaldark=dark.length;
+//     for (let i=0; i<totaldark; i++) {
+//         dark[i].addEventListener("click",function(){
+// console.log(this.value);
+
+//             // if (this.value === "dark") {
+//             //     document.body.className="dark";
+//             // }
+//             // else{
+//             //     document.body.className="";
+//             // }
+//         });
+//     }
+
+let dark;
+document.getElementById("dark",function(){
+    this.ad
+});
